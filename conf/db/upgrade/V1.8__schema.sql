7a1eb9a1b0a317e1b1bb51badc4b9f372e87f66d
Mei Lei <meilei007@gmail.com>
2016-11-06 17:48:29 +0800

    add importImageInfo field in database
    Signed-off-by: Mei Lei <meilei007@gmail.com>

--- conf/db/upgrade/V1.8__schema.sql
+++ conf/db/upgrade/V1.8__schema.sql
@@ -65,6 +65,9 @@ ALTER TABLE VCenterPrimaryStorageVO ADD CONSTRAINT fkVCenterPrimaryStorageVOVCen
 # Foreign keys for table VCenterVO
 ALTER TABLE ApplianceVmVO ADD agentPort int unsigned DEFAULT 7759;
 ALTER TABLE VCenterVO ADD CONSTRAINT fkVCenterVOZoneEO FOREIGN KEY (zoneUuid) REFERENCES ZoneEO (uuid) ON DELETE CASCADE  ;
+
 ALTER TABLE SchedulerVO CHANGE startDate  startTime  timestamp;
 ALTER TABLE SchedulerVO CHANGE stopDate  stopTime  timestamp NULL DEFAULT NULL;
 UPDATE SchedulerVO SET stopTime = NULL;
+
+ALTER TABLE `zstack`.`BackupStorageEO` add column importImageInfo bool default false;
